31b1bccfa5de6fff704051ce0eb54c5469b39d56
DavidPedrenho <davidpedrenoperez@gmail.com>
2024-05-06 18:32:53 +0200

    probando importar

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="mi-componente">
+    <h1>Este es mi componente</h1>
+    <h2>{{ subtitulo }}</h2>
+  </div>
+</template>
+
+<script>
+
+    export default{
+        name: 'MiComponente', //Nombre de la etiqueta
+
+        data() {
+            return{
+                subtitulo: 'Soy el subtitulo de mi componente'
+            }
+        },
+
+    // mounted() {
+    // fetch('http://localhost:4000/')
+    // .then(data => {
+    //     this.datos = data;
+    //   })
+        
+    // }
+
+  };
+
+</script>
