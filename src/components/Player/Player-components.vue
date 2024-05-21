165d90047d9b47ad7be76603511983796786e342
DavidPedrenho <davidpedrenoperez@gmail.com>
2024-05-21 22:41:38 +0200

    acaban card jugador, /pachanga

--- src/components/Player/Player-components.vue
+++ src/components/Player/Player-components.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <div class="card max-w-sm mx-auto w-96 border-4 shadow-slate-950 border-neutral bg-base-100 shadow-xl">
+    <div class="card max-w-sm mx-auto w-96  border-8  shadow-slate-950 border-neutral bg-base-100 shadow-xl">
       <figure class="px-10 pt-10">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhREmOESv8Q1TJ7Wwa38JHUSwVuk5v5EiAA&s"
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhREmOESv8Q1TJ7Wwa38JHUSwVuk5v5EiAA&s"
           alt="Shoes" class="border-4 border-neutral rounded-full" />
       </figure>
       <div class="p-4">
@@ -12,37 +11,51 @@
         </h1>
       </div>
 
-      <div class="items-center text-center  my-1" >
-        <span class="mx-3 justify-center" >
+      <div class="items-center text-center  text-gray-600">
+        <span class="mx-3 justify-center">
           Nº Partidos: 4
         </span>
-        
+
         <span class="mx-3 justify-center">
           MVP: 8
         </span>
       </div>
 
+      <!-- <div class="collapse bg-base-200">
+        <input type="checkbox" class="peer" />
+        <div
+          class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Click me to show/hide content
+        </div>
+        <div
+          class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>hello</p>
+        </div>
+      </div> -->
 
-      <div class="  join  lg:join-horizontal flex-auto flex justify-center">
 
-        <button class="btn tracking-wide w-1/2 join-item bg-red-400 hover:bg-red-500 text-base-200 font-extrabold py-2 px-4 ">
+      <div class="join lg:join-horizontal flex-auto flex justify-center rounded-none border-t-8 border-neutral">
+
+        <button
+          class="btn rounded-b-lg rounded-s-none tracking-wide w-1/2 join-item  text-sm bg-red-400 hover:bg-red-500 text-base-200 font-extrabold py-4 px-4 ">
           EQUIPO ROJO
         </button>
-        <button class=" btn tracking-wide w-1/2 join-item bg-blue-500 hover:bg-blue-600 text-base-300 font-extrabold py-2 px-4 ">
+        <button
+          class="btn rounded-br-lg rounded-r-none tracking-wide w-1/2 join-item text-sm bg-blue-500 hover:bg-blue-600 text-base-300 font-extrabold py-4 px-4 ">
           EQUIPO AZUL
         </button>
 
-        
+
       </div>
 
-      
+
     </div>
 
 
     <br>
     <br>
 
-    
+
   </div>
 </template>
 
